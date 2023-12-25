0c035123f3b5b5c9047d3946789c50657a7e8869
Andrey Danilenko <psoglav.ih8u@gmail.com>
2023-12-25 19:22:45 +0300

    fix pinia issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,9 +15,6 @@ export default defineNuxtConfig({
   imports: {
     dirs: ['stores']
   },
-  alias: {
-    pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs'
-  },
   app: {
     head: {
       titleTemplate: '%s / ASPIRYX',
