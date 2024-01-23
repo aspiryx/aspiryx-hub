ec45369e7de1c8dc3bfc46e965a06dfff8070f04
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-01-23 18:39:08 +0300

    some refactoring

---
+++ components/bio-type.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-max max-w-full relative group select-none flex flex-col items-center scale-75 md:scale-100">
+    <h1
+      class="username-back text-8xl text-black uppercase font-bold absolute left-1/2 -translate-x-1/2 translate-y-1/2 blur-sm"
+    >
+      ASPIRYX
+    </h1>
+    <h1 class="text-xl uppercase text-center tracking-wider z-10">
+      Andrey Danilenko
+    </h1>
+    <h1 class="username text-6xl text-white uppercase font-bold text-center z-10">
+      ASPIRYX
+    </h1>
+    <p class="w-full max-w-max uppercase font-normal text-opacity-50 text-white mt-3 text-center z-10">
+      Software Engineer | Visual Artist | Sound Researcher
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .username-back {
+    font-family: MetalMacabre;
+    filter: url(#filter-music);
+  }
+
+  .username {
+    font-family: Michroma;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -18,27 +18,12 @@ useHead({
           class="rounded-s-md"
           title="Github"
           to="https://github.com/psoglav"
-          subtitle="~/github"
+          subtitle="~/github/psoglav"
           target="_blank"
         />
       </bento-grid-row>
 
-      <div class="w-max max-w-full relative group select-none flex flex-col items-center scale-75 md:scale-100">
-        <h1
-          class="username-back text-8xl text-black uppercase font-bold absolute left-1/2 -translate-x-1/2 translate-y-1/2 blur-sm"
-        >
-          ASPIRYX
-        </h1>
-        <h1 class="text-xl uppercase text-center tracking-wider z-10">
-          Andrey Danilenko
-        </h1>
-        <h1 class="username text-6xl text-white uppercase font-bold text-center z-10">
-          ASPIRYX
-        </h1>
-        <p class="w-full max-w-max uppercase font-normal text-opacity-50 text-white mt-3 text-center z-10">
-          Software Engineer | Visual Artist | Sound Researcher
-        </p>
-      </div>
+      <bio-type />
     </section>
   </main>
 </template>
@@ -54,15 +39,6 @@ main {
 
   .hero {
     min-height: calc(100svh - $header-height);
-  }
-
-  .username-back {
-    font-family: MetalMacabre;
-    filter: url(#filter-music);
-  }
-
-  .username {
-    font-family: Michroma;
   }
 }
 </style>
