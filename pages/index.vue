d8a1ab54642bf7815c681a552113862a064a4d1c
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-09-18 16:21:18 +0300

    feat: add tg profile link

--- pages/index.vue
+++ pages/index.vue
@@ -6,37 +6,36 @@ useHead({
 
 <template>
   <main class="min-h-full">
-    <section class="hero flex flex-col justify-between py-24">
-      <bento-grid-row class="max-h-40 gap-4 invisible lg:visible">
+    <section class="hero flex flex-col min-h-screen relative items-center py-24 gap-6">
+      <bento-grid-row class="invisible lg:visible max-h-80">
         <bento-grid-item
           class="rounded-e-md"
           title="Projects"
           to="/projects"
           subtitle="~/projects"
         />
+      </bento-grid-row>
+      <bento-grid-row class="invisible lg:visible max-h-80">
         <bento-grid-item
           class="rounded-s-md"
-          title="Github"
+          title="Github Profile"
           to="https://github.com/aspiryx"
           subtitle="~/github/aspiryx"
           target="_blank"
         />
+        <bento-grid-item
+          class="rounded-e-md"
+          title="Telegram"
+          to="https://t.me/aspiryxx"
+          subtitle="~/tg/aspiryxx"
+        />
       </bento-grid-row>
-
-      <bio-type />
     </section>
   </main>
 </template>
 
 <style lang="scss" scoped>
 main {
-  section {
-    position: relative;
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-
   .hero {
     min-height: calc(100svh - $header-height);
   }
